f274f922fa4e1a9fba12884d56112345e81a9232
john makateto <jmakateto@gmail.com>
2023-11-21 17:44:03 +0300

    made changes

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from flask import Flask
 # Create an instance of the Flask class
 app = Flask(__name__)
 
+
 # Define a route for the homepage
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Welcome to my app!</h1>'
+    return "<h1>Welcome to my app!</h1>"
 
-# Define a route 
-@app.route('/<string:username>')
+
+# Define a route
+@app.route("/<string:username>")
 def user(username):
-    return f'<h1>Profile for {username}</h1>'
+    return f"<h1>Profile for {username}</h1>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555)
--- server/app.py
+++ server/app.py
@@ -1,8 +1,42 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<string:parameter>")
+def print_string(parameter):
+    print(parameter)
+    return f"<h1>{parameter}</h1>"
+
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    numbers = "\n".join(str(i) for i in range(parameter + 1))
+    return f"<pre>{numbers}</pre>"
+
+
+@app.route("/math/<int:num1><operation><int:num2>")
+def math(num1, operation, num2):
+    result = None
+
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "div":
+        result = num1 / num2
+    elif operation == "%":
+        result = num1 % num2
+
+    return f"<h1>Result: {result}</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
